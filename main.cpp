4abc860f18f6daf8b7751170454c014daf79e9f3
貝時計 KAITOKEI <m21148@g.metro-cit.ac.jp>
2024-12-03 11:00:30 +0900

    add: slide vertex index

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-#include <string>
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 
 #include "./vertex.cpp"
 #include "./utils.cpp"
@@ -29,6 +29,23 @@ int main(int argc, char *argv[])
 
     vector<Vertex> vtxs;                     // 頂点テーブル
     vtxs = read_graph_data(graph_data_path); // グラフデータを読み取る
+
+    // 公式に合わせるため、indexを+1ずらす
+    vtxs.insert(vtxs.begin(), Vertex(0, 0));
+    for (int i = 0; i < vtxs.size(); i++)
+        for (int j = 0; j < vtxs[i].to.size(); j++)
+            for (int k = 0; k < vtxs[i].to[j].size(); k++)
+                vtxs[i].to[j][k]++;
+
+    vector<vector<vector<string>>> regular(dim, vector<vector<string>>(dim, vector<string>(dim))); // 正規表現表
+
+    // 最小の状態推移を求める
+    for (int i = 1; i < vtxs.size(); i++)
+    {
+        for (int j = 0; j < vtxs[i].to.size(); j++)
+        {
+        }
+    }
 
     return 0;
 }

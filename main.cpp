5277f0dd35b86d50530b651e32b5f1729a61b77f
貝時計 KAITOKEI <m21148@g.metro-cit.ac.jp>
2024-12-03 12:53:22 +0900

    add: r0

--- main.cpp
+++ main.cpp
@@ -30,22 +30,48 @@ int main(int argc, char *argv[])
     vector<Vertex> vtxs;                     // 頂点テーブル
     vtxs = read_graph_data(graph_data_path); // グラフデータを読み取る
 
-    // 公式に合わせるため、indexを+1ずらす
-    vtxs.insert(vtxs.begin(), Vertex(0, 0));
-    for (int i = 0; i < vtxs.size(); i++)
-        for (int j = 0; j < vtxs[i].to.size(); j++)
-            for (int k = 0; k < vtxs[i].to[j].size(); k++)
-                vtxs[i].to[j][k]++;
-
-    vector<vector<vector<string>>> regular(dim, vector<vector<string>>(dim, vector<string>(dim))); // 正規表現表
+    vector<vector<vector<string>>> regular(vtxs.size(), vector<vector<string>>(vtxs.size(), vector<string>(vtxs.size(), ""))); // 正規表現表
 
     // 最小の状態推移を求める
-    for (int i = 1; i < vtxs.size(); i++)
+    for (int i = 0; i < vtxs.size(); i++)
     {
-        for (int j = 0; j < vtxs[i].to.size(); j++)
+        for (int j = 0; j < vtxs.size(); j++)
         {
+            for (const vector<int> &vec : vtxs[i].to)
+            {
+                for (int k = 0; k < vec.size(); k++)
+                {
+                    if (vec[k] == i)
+                    {
+                        regular[0][i][j] += " + " + to_string(k) + " + e";
+                    }
+                    else
+                    {
+                        regular[0][i][j] += " + " + to_string(k);
+                    }
+                }
+            }
         }
     }
+
+    // それぞれの推移の状態推移から正規表現を生成
+    for (int k = 1; k < vtxs.size(); k++)
+    {
+        for (int i = 1; i < vtxs.size(); i++)
+        {
+            for (int j = 1; j < vtxs.size(); j++)
+            {
+                for (const vector<int> &vec : vtxs[i].to)
+                {
+                    for (int k = 0; k < vec.size(); k++)
+                    {
+                    }
+                }
+            }
+        }
+    }
+
+    cout << regular[dim - 1][from - 1][to - 1] << endl;
 
     return 0;
 }
